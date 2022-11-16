e54736dfc3aec6ca52aa77971d545b648a70bb77
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-16 15:30:49 +0000

    Add passing test to check if quality updates correctly on normal items

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -6,5 +6,11 @@ describe('Gilded Rose', function () {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
+
+  it('quality updates correctly on a normal item', () => {
+    const gildedRose = new Shop([new Item('+5 Dexterity Vest', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(19);
+  });
 });
 // Footer;
