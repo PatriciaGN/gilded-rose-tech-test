5a95f7b0349c9a2a5389aede58f2ac09f1dca36d
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-16 15:42:52 +0000

    Add passing test for quality update when SellIn is zero

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -18,5 +18,27 @@ describe('Gilded Rose', function () {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(18);
   });
+
+  it('quality cannot be lower than zero', () => {
+    const gildedRose = new Shop([new Item('+5 Dexterity Vest', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('AgedBrie quality goes up instead of down as quality is updated', () => {
+    const gildedRose = new Shop([new Item('Aged Brie', 2, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
+  });
+
+  // AgedBrie
+
+  // Sulfuras
+
+  // Backstage passes
+
+  // Quality can't be higher than 50
+
+  // Conjured items
 });
 // Footer;
