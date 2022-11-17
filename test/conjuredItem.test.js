c630c82f366e6f47891b8d47dfa3e35f55996ccb
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-17 14:47:08 +0000

    Add second failing test for quality update on conjured items

--- test/conjuredItem.test.js
+++ test/conjuredItem.test.js
@@ -1,5 +1,4 @@
 const ConjuredItem = require('../src/conjuredItem');
-const { Item } = require('../src/gildedRose');
 
 describe('ConjuredItem', function () {
   it('degrades quality twice as fast', () => {
@@ -11,5 +10,17 @@ describe('ConjuredItem', function () {
     const conjuredItem = new ConjuredItem(fakeItem);
     conjuredItem.updateQualityConjuredItem();
     expect(conjuredItem.quality).toBe(4);
+  });
+
+  it('degrades quality twice as fast with several quality updates', () => {
+    let fakeItem = {
+      name: 'Conjured Mana Cake',
+      sellIn: 3,
+      quality: 6,
+    };
+    const conjuredItem = new ConjuredItem(fakeItem);
+    conjuredItem.updateQualityConjuredItem();
+    conjuredItem.updateQualityConjuredItem();
+    expect(conjuredItem.quality).toBe(2);
   });
 });
