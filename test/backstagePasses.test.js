c74db862359874d67decb755a804a3280ea6b2bd
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-17 12:18:12 +0000

    Add second failing test for quality update on BackstagePasses

--- test/backstagePasses.test.js
+++ test/backstagePasses.test.js
@@ -12,4 +12,18 @@ describe('BackstagePasses', function () {
     backstagePasses.updateQualityBackstagePasses();
     expect(backstagePasses.quality).toBe(21);
   });
+
+  it('increases quality when updated several times', () => {
+    let fakeItem = {
+      name: 'Backstage passes to a TAFKAL80ETC concert',
+      sellIn: 15,
+      quality: 20,
+    };
+    const backstagePasses = new BackstagePasses(fakeItem);
+    backstagePasses.updateQualityBackstagePasses();
+    backstagePasses.updateQualityBackstagePasses();
+    expect(backstagePasses.quality).toBe(22);
+  });
+
+  // No further increase if over 50
 });
