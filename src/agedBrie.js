847dd9d7581b5b6a5763a14a16b4d10eed862fdc
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-17 11:03:19 +0000

    Create AgedBrie class and updateQualityAgedBrie() to pass first test

---
+++ src/agedBrie.js
@@ -0,0 +1,13 @@
+const { Item } = require('../src/gildedRose');
+
+class AgedBrie {
+  constructor(item) {
+    this.sellIn = item.sellIn;
+    this.quality = item.quality;
+  }
+  updateQualityAgedBrie() {
+    this.quality = 1;
+  }
+}
+
+module.exports = AgedBrie;
