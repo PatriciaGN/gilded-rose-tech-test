a951a0843e5cd7888dfb09641e987c753f241ca5
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-17 12:06:27 +0000

    Create Sulfuras class and updateQualitySulfuras() to pass test

---
+++ src/sulfuras.js
@@ -0,0 +1,13 @@
+const { Item } = require('../src/gildedRose');
+
+class Sulfuras {
+  constructor(item) {
+    this.sellIn = item.sellIn;
+    this.quality = item.quality;
+  }
+  updateQualitySulfuras() {
+    return;
+  }
+}
+
+module.exports = Sulfuras;
