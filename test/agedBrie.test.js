5020f9c9f094dc983f97fe975f6b577611e2a2ff
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-17 11:04:50 +0000

    Add second failing test for quality updates

--- test/agedBrie.test.js
+++ test/agedBrie.test.js
@@ -2,7 +2,7 @@ const { Item } = require('../src/gildedRose');
 const AgedBrie = require('../src/agedBrie');
 
 describe('AgedBrie', function () {
-  it('quality increases by one when updated', () => {
+  it('increases quality by one when updated once', () => {
     let fakeItem = {
       name: 'AgedBrie',
       sellIn: 2,
@@ -11,5 +11,17 @@ describe('AgedBrie', function () {
     const agedBrie = new AgedBrie(fakeItem);
     agedBrie.updateQualityAgedBrie();
     expect(agedBrie.quality).toBe(1);
+  });
+
+  it('increases quality by one every time it is updated', () => {
+    let fakeItem = {
+      name: 'AgedBrie',
+      sellIn: 2,
+      quality: 0,
+    };
+    const agedBrie = new AgedBrie(fakeItem);
+    agedBrie.updateQualityAgedBrie();
+    agedBrie.updateQualityAgedBrie();
+    expect(agedBrie.quality).toBe(2);
   });
 });
